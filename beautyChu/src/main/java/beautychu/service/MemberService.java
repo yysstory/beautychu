fd9b6598f3c005e77d3e5936b5cc66e341ba6dd1
yun <yun@yun-PC>
2015-01-05 13:16:57 +0900

    ..

---
+++ beautyChu/src/main/java/beautychu/service/MemberService.java
@@ -0,0 +1,28 @@
+package beautychu.service;
+
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import beautychu.dao.MemberDao;
+import beautychu.domain.Member;
+
+@Service
+public class MemberService {
+  @Autowired MemberDao memberDao;
+  
+/*  public Member selectOne(int no) {
+    return memberDao.selectOne(no); 
+  }*/
+  
+  
+  public Member validate(String email, String password) {
+	    HashMap<String,String> params = new HashMap<>();
+	    params.put("email", email);
+	    params.put("password", password);
+	    return memberDao.existUser(params);
+	  }
+}
+
+
